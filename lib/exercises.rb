ec72182aa50b722a10288a7d879ea2d5d24f1bde
Maryam Heshmati <mairheshmati@gmail.com>
2020-09-10 16:10:37 -0700

    implemented grouped_anagrams methods

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,11 +1,30 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * m) where n is word and m is char 
+# Space Complexity: O(n)
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  result = []
+  return result if strings.empty?
+
+  word_hash = {}
+  strings.each do |word|
+    char_hash = {}
+    word.each_char do |char|
+      if char_hash[char]
+        char_hash[char] += 1
+      else
+        char_hash[char] = 1
+      end
+    end
+    if word_hash[char_hash]
+      word_hash[char_hash] << word
+    else
+      word_hash[char_hash] = [ word ]
+    end
+  end
+  return word_hash.values
 end
 
 # This method will return the k most common elements
