2c6310282cc70b6724dab5c74f3950842a24273c
Hannah <hannahjoo24@gmail.com>
2020-09-13 13:51:56 -0700

    Refactor valid_sudoku method

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -92,9 +92,6 @@ def valid_sudoku(table)
       j += 1
     end
 
-    puts "row: #{row_list}"
-    puts "col: #{col_list}"
-
 
     row_list = {}
     col_list = {}
@@ -103,72 +100,24 @@ def valid_sudoku(table)
   end
 
 
-  sub_grid = {}
-  sub_grid = valid_sub_grid(table, sub_grid)
-  return false if !sub_grid
+  starting_points = [
+    [0, 0],
+    [0, 3],
+    [0, 6],
+    [3, 0],
+    [3, 3],
+    [3, 6],
+    [6, 0],
+    [6, 3],
+    [6, 6]
+  ]
 
-  return true
-end
-
-
-def valid_sub_grid(table, sub_grid)
-  i = 0
-  while i < table.length
-    n = 0
-    while n < table[i].length
-
-      num1 = table[i][n]
-      num2 = table[i][n + 1]
-      num3 = table[i][n + 2]
-
-      num4 = table[i + 1][n]
-      num5 = table[i + 1][n + 1]
-      num6 = table[i + 1][n + 2]
-
-      num7 = table[i + 2][n]
-      num8 = table[i + 2][n + 1]
-      num9 = table[i + 2][n + 2]
-
-      sub_grid = update_sub_grid(sub_grid, num1)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num2)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num3)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num4)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num5)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num6)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num7)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num8)
-      return false if !sub_grid
-
-      sub_grid = update_sub_grid(sub_grid, num9)
-      return false if !sub_grid
-
-
-      puts "sub_grid: #{sub_grid}"
-      sub_grid = {}
-
-      n += 3
-
-    end 
-
-    i += 3
+  starting_points.each do |row_start, col_start|
+    return false if !valid_sub_grid(table, row_start, col_start)
   end 
 
   return true
-end 
+end
 
 
 def update_sub_grid(sub_grid, num)
@@ -181,52 +130,20 @@ def update_sub_grid(sub_grid, num)
   return sub_grid
 end
 
+def valid_sub_grid(table, row_start, col_start)
+  sub_grid = {}
 
+  3.times do |r|
+    3.times do |c|
+      current_num = table[row_start + r][col_start + c]
 
-# 012 / 345 / 678
+      if sub_grid[current_num] && current_num != "."
+        return false 
+      else 
+        sub_grid[current_num] = true 
+      end 
+    end 
+  end
 
-
-# e.g.
-# table = [
-#         ["5","3",".",".","7",".",".",".","."],
-#         ["6",".","3","1","9","5",".",".","."],
-#         [".","9","8",".",".",".",".","6","."],
-#         ["8",".",".",".","6",".",".",".","3"],
-#         ["4",".",".","8",".","3",".",".","1"],
-#         ["7",".",".",".","2",".",".",".","6"],
-#         [".","6",".",".",".",".","2","8","."],
-#         [".",".",".","4","1","9",".",".","5"],
-#         [".",".",".",".","8",".",".","7","9"]
-#       ]
-
-# 1st
-# 00 / 01 / 02
-# 10 / 11 / 12
-# 20 / 21 / 22
-
-# 30 / 31 / 32
-# 40 / 41 / 42
-# 50 / 51 / 52
-
-# 60 / 61 / 62
-# 70 / 71 / 72
-# 80 / 81 / 82
-
-
-# 03 / 04 / 05
-# 13 / 14 / 15
-# 23 / 24 / 25
-
-# 33 / 34 / 35
-# 43 / 44 / 45
-# 53 / 54 / 55
-
-
-# 06 / 07 / 08
-# 16 / 17 / 18
-# 26 / 27 / 28
-
-
-# 36 / 37 / 38
-# 46 / 47 / 48
-# 56 / 57 / 58
+  return true
+end 
