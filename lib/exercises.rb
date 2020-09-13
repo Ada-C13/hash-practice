948e7492014f4e965b12b3c4796688650e1d1d50
SuelyBarreto <suely@barreto.us>
2020-09-12 18:05:10 -0700

    tests passing

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,29 +1,50 @@
-
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) + sorting
+# Space Complexity: O(n)
 
-def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+def grouped_anagrams(strings) 
+  h = {}
+  return [] if strings.nil? || strings.empty? || !strings.is_a?(Array)
+  strings.each do |str|   
+    key = str.chars.sort.join
+    if h[key].nil?
+      h[key] = [str]
+    else
+      h[key] << str
+    end 
+  end
+  p h.values
+  return h.values
 end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
-end
-
-
-# This method will return the true if the table is still
-#   a valid sudoku table.
-#   Each element can either be a ".", or a digit 1-9
-#   The same digit cannot appear twice or more in the same 
-#   row, column or 3x3 subgrid
-# Time Complexity: ?
-# Space Complexity: ?
-def valid_sudoku(table)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
-end
+  h = {}
+  solution = []
+  return solution if list.nil? || list.empty? || !list.is_a?(Array)
+  list.each do |element|
+    if h[element].nil?
+      h[element] = 1
+    else
+      h[element] += 1
+    end
+  end
+  p h
+  k.times do
+    top = nil
+    h.each do |element,value|  
+      if value && (top.nil? || value > top)
+        top = element
+      end
+    end 
+    if top 
+      h[top] = nil
+      solution << top
+    end
+  end 
+  return solution
+end 
