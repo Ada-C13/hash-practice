2887444fae0605fbdbcc2e5a0ae2b3b135d1e59b
peachmakkoli <lee.higgins.dev@gmail.com>
2020-09-15 08:21:00 -0700

    adds comments for time and space complexity

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -21,8 +21,8 @@ end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n), where n is the number of elements
+# Space Complexity: O(1)?
 def top_k_frequent_elements(list, k)
   return list if list.length < 2
 
