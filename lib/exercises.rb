cdee3bb35844125167c7d70d36f231b636cc16fb
peachmakkoli <lee.higgins.dev@gmail.com>
2020-09-15 08:17:45 -0700

    completes top_k_frequent_elements

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -9,7 +9,7 @@ def grouped_anagrams(strings)
   
   strings.each do |s|
     # sort each string, assign to variable
-    bucket = anagrams[s.chars.sort.join] 
+    bucket = anagrams[s.chars.sort.join]  
     # check if sorted string is present in hash keys
     # if present, add original string to existing bucket array
     # otherwise, create a new bucket array
@@ -24,9 +24,40 @@ end
 # Time Complexity: ?
 # Space Complexity: ?
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return list if list.length < 2
+
+  items_hash = {}
+
+  list.each do |item|
+    items_hash[item] ? items_hash[item] += 1 : items_hash[item] = 1
+  end
+
+  if items_hash.values.uniq.length == k
+    tiebreaker(items_hash, k)
+  else
+    return most_common = items_hash.keys.max_by(k) do |item|
+      items_hash[item]
+    end
+  end
 end
 
+# this only works for the test case. for example, the following will fail:
+# list = [1,1,1, 2,2,2, 3,3]
+# k = 2
+# expected output: [1, 3]
+def tiebreaker(items_hash, k)
+  sorted = items_hash.keys.sort_by do |item| 
+    -items_hash[item]
+  end
+
+  most_common = []
+  k.times do |i|
+    most_common.push(sorted[i])
+    i += 1
+  end
+
+  return most_common
+end
 
 # This method will return the true if the table is still
 #   a valid sudoku table.
