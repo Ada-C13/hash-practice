81b1c528339f1079e35b32db9010108b7519dfd5
Stephanie <stephaniejmars@gmail.com>
2020-09-27 12:21:42 -0700

    added bigO

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -20,8 +20,8 @@ end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
 def top_k_frequent_elements(list, k)
   return [] if list == []
   hash = {}
